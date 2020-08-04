6f532a340fc47489f1ef784397f5d93012ff9264
Leon Arndt <neotalon27@gmail.com>
2020-08-04 13:26:56 +0200

    Add about route

---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Test Hallo About</h1>
+  </div>
+</template>
---
+++ src/views/Route66.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="route66">
+    <h1>Route 66 is 3,940 km long</h1>
+  </div>
+</template>
